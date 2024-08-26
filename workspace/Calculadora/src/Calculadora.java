4ca6abb0307180f4f83ebc0ad1581786b8aea0f3
hugo <hugocastanedahernandez1@gmail.com>
2024-08-26 01:22:07 -0600

    Se carga los archivos de la ultima semana ;,)

---
+++ workspace/Calculadora/src/Calculadora.java
@@ -0,0 +1,44 @@
+public class Calculadora implements ICalculadora {
+
+    @Override
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+
+    @Override
+    public int restar(int a, int b) {
+        return a - b;
+    }
+
+    public double restar(double a, double b) {
+        return a - b;
+    }
+
+    @Override
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    @Override
+    public int dividir(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }//exception
+        return a / b;
+    }
+    
+    public double dividir(double a, double b) throws ArithmeticException {
+        if (b == 0.0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }//exception
+        return a / b;
+    }
+}//class Claculadorra
---
+++ workspace/Calculadora/src/ICalculadora.java
@@ -0,0 +1,12 @@
+public interface ICalculadora {
+	
+	int sumar(int a, int b);
+
+	int restar(int a, int b);
+
+	int multiplicar(int a, int b);
+
+	int dividir(int a, int b) throws Exception;
+
+	
+}// interface
